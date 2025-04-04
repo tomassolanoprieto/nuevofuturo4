30d4890fc8b003226d53510ddf921fdd93e3c6be
tomassolanoprieto <162082823+tomassolanoprieto@users.noreply.github.com>
2025-04-04 16:15:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Habilita el history API fallback para desarrollo
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'), // Asegura el manejo correcto de rutas en producción
+      },
+    },
+    outDir: 'dist', // Asegura que la build vaya a la carpeta que Netlify espera
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
